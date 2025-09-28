5cc4f9a360b76fe51f824c241b2491d851d25a02
Никита Волков <Tyuiu.VolkovNS@outlook.com>
2025-09-28 12:11:41 +0300

    Task4

---
+++ Tyuiu.VolkovNS.Sprint1.Task4.V12.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.VolkovNS.Sprint1.Task4.V12.Lib;
+
+namespace Tyuiu.VolkovNS.Sprint1.Task4.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x=1,y=1;
+            var res = Math.Round(ds.Calculate(x,y),3);
+            Assert.AreEqual(0, res);
+        }
+    }
+}
---
+++ Tyuiu.VolkovNS.Sprint1.Task4.V12/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.VolkovNS.Sprint1.Task4.V12.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнил Волков Н. С. | ПКТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Class Math                                                              *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #12                                                             *");
+        Console.WriteLine("* Выполнил: Волков Никита Сергеевич | ПКТб-25-1                           *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        double x, y;
+        Console.WriteLine("Введите X: ");
+        x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите Y: ");
+        y = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Ответ равен: " + Math.Round(ds.Calculate(x,y),3));
+    }
+}
