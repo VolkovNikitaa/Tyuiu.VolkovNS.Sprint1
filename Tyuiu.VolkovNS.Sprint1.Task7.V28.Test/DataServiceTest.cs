dc5520b142ab8a8e6c4fad121ed3d4d2dd470d5c
Никита Волков <Tyuiu.VolkovNS@outlook.com>
2025-09-28 14:07:22 +0300

    Task7

---
+++ Tyuiu.VolkovNS.Sprint1.Task7.V28.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.VolkovNS.Sprint1.Task7.V28.Lib;
+
+namespace Tyuiu.VolkovNS.Sprint1.Task7.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x=1, y=2, wait = 3;
+            Assert.AreEqual(wait,ds.Calculate(x,y));
+        }
+    }
+}
---
+++ Tyuiu.VolkovNS.Sprint1.Task7.V28/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.VolkovNS.Sprint1.Task7.V28.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        //       2     /1\      x
+        //z = cos (sin|---|) + y
+        //             \x/ 
+        Console.Title = "Спринт #1 | Выполнил Волков Н. С. | ПКТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #28                                                             *");
+        Console.WriteLine("* Выполнил: Волков Никита Сергеевич | ПКТб-25-1                           *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет математическое выражение          *");
+        Console.WriteLine("* по исходным значениям данных, вводимых пользователем.                   *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("*        2     /1\\      x                                                *");
+        Console.WriteLine("* z = cos (sin|---|) + y                                                  *");
+        Console.WriteLine("*              \\x/                                                       *");
+
+        double x, y;
+        Console.WriteLine("Введите X: ");
+        x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите Y: ");
+        y = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Ответ равен: " + ds.Calculate(x,y));
+    }
+}
