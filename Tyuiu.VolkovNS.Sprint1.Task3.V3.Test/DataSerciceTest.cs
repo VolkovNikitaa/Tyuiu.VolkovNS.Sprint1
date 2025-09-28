b917f02cf475e5f48fde1d76f122d09c7267a793
Никита Волков <Tyuiu.VolkovNS@outlook.com>
2025-09-28 11:28:13 +0300

    Task3

---
+++ Tyuiu.VolkovNS.Sprint1.Task3.V3.Test/DataSerciceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.VolkovNS.Sprint1.Task3.V3.Lib;
+namespace Tyuiu.VolkovNS.Sprint1.Task3.V3.Test
+{
+    [TestClass]
+    public sealed class DataSerciceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double lenght = 9, width = 7.5, height = 5;
+            var res = ds.ParallelepipedVolume(lenght,width,height);
+            Assert.AreEqual(337.5, res);
+        }
+    }
+}
---
+++ Tyuiu.VolkovNS.Sprint1.Task3.V3/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.VolkovNS.Sprint1.Task3.V3.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнил Волков Н. С. | ПКТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #11                                                             *");
+        Console.WriteLine("* Выполнил: Волков Никита Сергеевич | ПКТб-25-1                           *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу вычисления объема параллелепипеда.                   *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        double lenght, width, height;
+        Console.WriteLine("Введите длину параллелепипеда: ");
+        lenght = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите ширину параллелепипеда: ");
+        width = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите высоту параллелепипеда: ");
+        height = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Объём параллелепипеда равен: " + Math.Round(ds.ParallelepipedVolume(lenght,width,height), 3));
+    }
+}
